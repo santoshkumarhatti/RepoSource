9e4e4a83e845f2dd8927d6de06a26175243d73a8
Santosh Kumar Hatti <sndsanthosh74@gmail.com>
2025-07-08 14:43:50 +0000

    still small

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default async function HomePage() {
       </header>
 
       <main className="flex-grow">
-        <section className="container mx-auto px-4 py-6">
+        <section className="container mx-auto px-4 py-2">
            <BannerCarousel banners={banners} />
         </section>
 
