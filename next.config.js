70cf5eb4691024069d78f4820f4c81e3af7af9e4
Santosh Kumar Hatti <sndsanthosh74@gmail.com>
2025-07-09 09:40:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -21,6 +21,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.videolan.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
